25ae1d07b6ece70e584e4b4e1794125d0fc285ca
Mo Yang <moyang313@gmail.com>
2022-03-12 16:34:34 -0500

    add function to load stored calendar info from local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,3 +7,18 @@ var hour = $(this).parent().attr("id");
 var description = $(this).siblings(".description").val();
 localStorage.setItem(hour, description);
 })
+
+//retrieves calendar from local storage
+var loadCalendar = function() {
+    $('#9am .description').val(localStorage.getItem("9am"))
+    $('#10am .description').val(localStorage.getItem("10am"))
+    $('#11am .description').val(localStorage.getItem("11am"))
+    $('#12pm .description').val(localStorage.getItem("12pm"))
+    $('#1pm .description').val(localStorage.getItem("1pm"))
+    $('#2pm .description').val(localStorage.getItem("2pm"))
+    $('#3pm .description').val(localStorage.getItem("3pm"))
+    $('#4pm .description').val(localStorage.getItem("4pm"))
+    $('#5pm .description').val(localStorage.getItem("5pm"))
+}
+
+loadCalendar();
