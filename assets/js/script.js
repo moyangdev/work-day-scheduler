a43fff132b52aba783fdbe5afdb7b06903d529c1
Mo Yang <moyang313@gmail.com>
2022-03-12 16:03:57 -0500

    add function to store info in local storage when save button is clicked

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,9 @@
+//formats current day and date
 $("#currentDay").text(moment().format('dddd, MMMM Do'));
+
+//when save button is clicked, sets hour id and description value in local storage
+$(".saveBtn").on("click", function() {
+var hour = $(this).parent().attr("id");
+var description = $(this).siblings(".description").val();
+localStorage.setItem(hour, description);
+})
